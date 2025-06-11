8fe7efec8ad91709ad6b3bc19664410a629c6d2e
anhbn <anhbach043@gmail.com>
2025-06-11 15:26:14 +0700

    .

--- src/App.tsx
+++ src/App.tsx
@@ -14,40 +14,56 @@ import { ScrollToTop } from "./components/common/ScrollToTop";
 import Homes from "./pages/Dashboard/Homes";
 import { ProtectedRoutes } from "./ProtectedRoutes";
 import { Store } from "./pages/Store";
+import routes from "./config/routes";
+import CreateEmployeeForm from "./pages/Hrm/CreateEmp";
 // import PrivateRoute from "./components/common/PrivateRoute";
 
 export default function App() {
-  return (
-    <>
-      <Router>
-        <ScrollToTop />
-        <Routes>
+    return (
+        <>
+            <Router>
+                <ScrollToTop />
+                <Routes>
+                    {/* Auth and Puclic Routes */}
+                    <Route path="/signin" element={<SignIn />} />
+                    <Route path="/signup" element={<SignUp />} />
+                    <Route path="*" element={<NotFound />} />
 
-           {/* Auth and Puclic Routes */}
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="*" element={<NotFound />} />
-
-
-          {/* Protected Routes */}
-          {/* Dashboard Layout */}
-          <Route  element={<ProtectedRoutes allowedRoles={['ROLE_ADMIN', 'ROLE_EMPLOYEE']} />}>
-            <Route element={<AppLayout />}>
-            <Route index path="/homes" element={<Homes />} />
-            {/* Others Page */}
-            <Route path="/profile" element={<UserProfiles />} />
-            <Route path="/calendar" element={<Calendar />} />
-            {/* Forms */}
-            <Route path="/form-elements" element={<FormElements />} />
-            {/* Tables */}
-            <Route path="/basic-tables" element={<BasicTables />} />
-            {/* Store */}
-            <Route path="/store" element={<Store/>} />
-          </Route>
-          </Route>
-
-        </Routes>
-      </Router>
-    </>
-  );
+                    {/* Protected Routes */}
+                    {/* Dashboard Layout */}
+                    <Route
+                        element={
+                            <ProtectedRoutes
+                                allowedRoles={["ROLE_ADMIN", "ROLE_EMPLOYEE"]}
+                            />
+                        }
+                    >
+                        <Route element={<AppLayout />}>
+                            <Route index path="/homes" element={<Homes />} />
+                            {/* Others Page */}
+                            <Route path="/profile" element={<UserProfiles />} />
+                            <Route path="/calendar" element={<Calendar />} />
+                            {/* Forms */}
+                            <Route
+                                path="/form-elements"
+                                element={<FormElements />}
+                            />
+                            {/* Tables */}
+                            <Route
+                                path="/basic-tables"
+                                element={<BasicTables />}
+                            />
+                            {/* Store */}
+                            <Route path="/store" element={<Store />} />
+                            {/* Human Resources Management */}
+                            <Route
+                                path={routes.ce}
+                                element={<CreateEmployeeForm />}
+                            />
+                        </Route>
+                    </Route>
+                </Routes>
+            </Router>
+        </>
+    );
 }
